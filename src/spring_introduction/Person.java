8fa21b58557d28b479d2c7a1972a95c440187922
pulkras <maligin128@gmail.com>
2023-02-01 23:32:36 +0300

    DI with values

--- src/spring_introduction/Person.java
+++ src/spring_introduction/Person.java
@@ -2,6 +2,8 @@ package spring_introduction;
 
 public class Person {
     private Pet pet;
+    private String surname;
+    private int age;
 
 //    public Person(Pet pet) {
 //        System.out.println("Person is created");
@@ -16,6 +18,25 @@ public class Person {
         System.out.println("Class Person: set pet");
         this.pet = pet;
     }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        System.out.println("Class person: set surname");
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Class person: set age");
+        this.age = age;
+    }
+
     public void callYourPet() {
         System.out.println("Hello, my lovely Pet!");
         pet.say();
