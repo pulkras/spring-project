92274b11d26aa6004b1a105bef236c8351cf8ba7
pulkras <maligin128@gmail.com>
2023-02-02 23:20:05 +0300

    prototype

--- src/spring_introduction/Test4.java
+++ src/spring_introduction/Test4.java
@@ -4,6 +4,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test4 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext2.xml");
+
+
         Dog myDog = context.getBean("myPet", Dog.class);
         myDog.setName("Kukuma");
         Dog yourDog = context.getBean("myPet", Dog.class);
