bcf785bcfe86a9e4f8113893c99f401459e9b5da
pulkras <maligin128@gmail.com>
2023-02-07 16:35:26 +0300

    java configuration and start of aop

--- src/spring_introduction/MyConfig.java
+++ src/spring_introduction/MyConfig.java
@@ -1,9 +1,22 @@
 package spring_introduction;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 
 @Configuration
-@ComponentScan("spring_introduction")
+@PropertySource("classpath:myApp.properties")
+// @ComponentScan("spring_introduction")
 public class MyConfig {
+
+    @Bean
+    @Scope("singleton")
+    public Pet catBean() {
+        System.out.println("!!!");
+        return new Cat();
+    }
+
+    @Bean
+    @Scope("prototype")
+    public Person personBean() {
+        return new Person(catBean());
+    }
 }
--- src/spring_introduction/Test6.java
+++ src/spring_introduction/Test6.java
@@ -6,10 +6,16 @@ public class Test6 {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
 
-        Person person = context.getBean("personBean", Person.class);
+//        Pet cat1 = context.getBean("catBean", Pet.class);
+//        Pet cat2 = context.getBean("catBean", Pet.class);
+//        cat.say();
 
-        person.callYourPet();
+        Person person1 = context.getBean("personBean", Person.class);
+        Person person2 = context.getBean("personBean", Person.class);
 
+        person1.callYourPet();
+        System.out.println(person1.getSurname());
+        System.out.println(person2.getAge());
         context.close();
     }
 }
