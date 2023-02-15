057062e9af6bbb956b0b05cb41933fc77eed8e1f
pulkras <maligin128@gmail.com>
2023-02-15 23:43:16 +0300

    hibernate connection

---
+++ src/hibernate_test/Test1.java
@@ -0,0 +1,27 @@
+package hibernate_test;
+
+import hibernate_test.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test1 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+
+        try {
+            Session session = factory.getCurrentSession();
+            Employee employee = new Employee("Sasha", "Kalishik", "IT", 500);
+            session.beginTransaction();
+            session.save(employee);
+            session.getTransaction().commit();
+        } finally {
+            factory.close();
+        }
+
+
+    }
+}
