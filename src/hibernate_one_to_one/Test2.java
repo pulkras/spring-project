ee26692402ba9f0913dbe43464ca9888f11f092e
pulkras <maligin128@gmail.com>
2023-02-20 23:23:12 +0300

    one to many uni and bi

--- src/hibernate_one_to_one/Test2.java
+++ src/hibernate_one_to_one/Test2.java
@@ -1,7 +1,7 @@
-package hibernate_test2;
+package hibernate_one_to_one;
 
-import hibernate_test2.entity.Detail;
-import hibernate_test2.entity.Employee;
+import hibernate_one_to_one.entity.Detail;
+import hibernate_one_to_one.entity.Employee;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -22,7 +22,7 @@ public class Test2 {
 //
 //            employee.setEmployeeDetail(detail);
 //            detail.setEmployee(employee);
-
+//********************************************
             session.beginTransaction();
 
             Detail detail = session.get(Detail.class, 1);
